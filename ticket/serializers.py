9406f902763392cd3d0d63b9840eee2dff9d6027
atheermusa <44138770+atheermusa@users.noreply.github.com>
2022-12-11 18:22:27 +0000

    add Ticket serializer

--- ticket/serializers.py
+++ ticket/serializers.py
@@ -6,7 +6,13 @@ from . import models
 class TicketTypeSerializer(serializers.ModelSerializer):
     class Meta:
         model = models.TicketType
-        fields = ('id', 'name')
+        fields = ("id", "name")
+
+
+class TicketSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = models.Ticket
+        fields = ("ticket_type", "order")
 
 
 class EventSerializer(serializers.ModelSerializer):
@@ -14,10 +20,10 @@ class EventSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = models.Event
-        fields = ('id', 'name', 'description', 'ticket_types')
+        fields = ("id", "name", "description", "ticket_types")
 
 
 class OrderSerializer(serializers.ModelSerializer):
     class Meta:
         model = models.Order
-        fields = ('id', 'ticket_type', 'quantity')
+        fields = ("id", "ticket_type", "quantity")
