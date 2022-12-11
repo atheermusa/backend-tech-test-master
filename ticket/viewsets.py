3c298f05b4e88478dc0ee7cde1d05dde5aee7e82
atheermusa <44138770+atheermusa@users.noreply.github.com>
2022-12-11 18:22:53 +0000

    (draft) update viewsets

--- ticket/viewsets.py
+++ ticket/viewsets.py
@@ -1,12 +1,63 @@
-from rest_framework import mixins, viewsets, exceptions
+from rest_framework import mixins, viewsets, exceptions, response, status
 
 from .models import Event, TicketType, Order
 from .serializers import EventSerializer, TicketTypeSerializer, OrderSerializer
+from rest_framework.mixins import CreateModelMixin, ListModelMixin, RetrieveModelMixin
 
 
-class EventViewSet(viewsets.ReadOnlyModelViewSet):
+class EventViewSet(
+    CreateModelMixin, ListModelMixin, RetrieveModelMixin, viewsets.GenericViewSet
+):
     serializer_class = EventSerializer
-    queryset = Event.objects.prefetch_related('ticket_types')
+    queryset = Event.objects.prefetch_related("ticket_types")
+
+    def create(self, request):
+        # Get the data for the new event and order from the request
+        event_data = request.data.get("event")
+        order_data = request.data.get("order")
+
+        # Create a new event instance
+        event = Event.objects.create(
+            name=event_data.get("name"), description=event_data.get("description")
+        )
+
+        # Create a new order instance for the event
+        order = Order.objects.create(
+            user=order_data.get("user"),
+            ticket_type=order_data.get("ticket_type"),
+            quantity=order_data.get("quantity"),
+        )
+
+        # Serialize the event and order instances and return the response
+        event_serializer = EventSerializer(event)
+        order_serializer = OrderSerializer(order)
+        data = {"event": event_serializer.data, "order": order_serializer.data}
+        return response.Response(data, status=status.HTTP_201_CREATED)
+
+    def list(self, request):
+        # Get the queryset of events
+        queryset = self.get_queryset()
+
+        # Add the extra information to each event instance in the queryset
+        for event in queryset:
+            # Get the number of orders and cancellation rate for the event
+            total_orders, cancellation_rate = event.get_orders_and_cancellation_rate()
+
+            # Get the date with the highest number of cancelled tickets for the event
+            date_with_highest_cancelled_tickets = (
+                event.get_date_with_highest_number_of_cancelled_tickets()
+            )
+
+            # Add the extra information to the event instance
+            event.total_orders = total_orders
+            event.cancellation_rate = cancellation_rate
+            event.date_with_highest_cancelled_tickets = (
+                date_with_highest_cancelled_tickets
+            )
+
+        # Serialize the queryset and return the response
+        serializer = self.get_serializer(queryset, many=True)
+        return response.Response(serializer.data)
 
 
 class OrderViewSet(mixins.CreateModelMixin, viewsets.ReadOnlyModelViewSet):
